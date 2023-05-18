072d09b838ca4857b735aa1ac296287d68064443
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-05-18 14:46:29 +0100

    AMB-1519: Added/updated tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -68,25 +68,37 @@ def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
 
 
+@pytest.mark.auth
+@pytest.mark.integration
+@pytest.mark.user_restricted_separate_nhs_login
 @pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level0"})
-def test_app_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-    assert resp.status_code == 401  # unauthorized
+def test_auth_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {"Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1"}
+    headers.update(nhsd_apim_auth_headers)
+
+    resp = requests.get(f"{nhsd_apim_proxy_url}/documents/Patient/9000000009/MedicationStatement", headers=headers)
+    assert resp.status_code == 401
 
 
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_app_level3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
-    assert resp.status_code == 200
-
-
+@pytest.mark.auth
 @pytest.mark.nhsd_apim_authorization(
     {
-        "access": "healthcare_worker",
-        "level": "aal3",
-        "login_form": {"username": "656005750104"},
+        "access": "patient",
+        "level": "P9",
+        "login_form": {"username": "9912003071"}
     }
 )
-def test_cis2_aal3(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
+def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {
+        "Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1",
+        "accept": "application/fhir+json",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
+    }
+    headers.update(nhsd_apim_auth_headers)
+
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/documents/Patient/9000000009/MedicationStatement",
+        headers=headers
+    )
     assert resp.status_code == 200
