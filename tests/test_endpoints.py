cc3df1aaab8ebec93c3a38c2a87254e26e76750c
almas-liaqat <Almas.liaqat@accentre.com>
2023-05-31 17:51:38 +0100

    added extrnal file in OAS schema ref + added test

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -4,7 +4,7 @@ https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
 from os import getenv
-
+from .example_loader import load_example
 import pytest
 import requests
 
@@ -87,3 +87,20 @@ def test_app_level0(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
 def test_nhs_login_p9(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     resp = requests.get(f"{nhsd_apim_proxy_url}", headers=nhsd_apim_auth_headers)
     assert resp.status_code == 200
+
+
+def test_prism_returns_external_file(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    headers = {
+        "Interaction-ID": "urn:nhs:names:services:gpconnect:fhir:operation:gpc.getstructuredrecord-1",
+        "accept": "*/*",
+        "X-Correlation-ID": "11C46F5F-CDEF-4865-94B2-0EE0EDCC26DA",
+        "X-Request-ID": "60E0B220-8136-4CA5-AE46-1D97EF59D068"
+    }
+    headers.update(nhsd_apim_auth_headers)
+
+    resp = requests.get(
+        f"{nhsd_apim_proxy_url}/Patient/9000000009/MedicationRequest",
+        headers=headers
+    )
+    expected_response = load_example("GetResponsePreviousPrescriptionDetailed.yaml")
+    assert resp.json() == expected_response.get("value")
